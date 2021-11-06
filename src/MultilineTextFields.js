f7c5910c5fb12859b08af8a62538cd15cac466a8
natkai2040 <72627669+natkai2040@users.noreply.github.com>
2021-11-06 16:49:32 -0400

    Create MultilineTextFields.js

---
+++ src/MultilineTextFields.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+export default function MultilineTextFields() {
+  const [value, setValue] = React.useState('Paste Schedule Here');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={10}
+          value={value}
+          onChange={handleChange}
+        />
+      </div>
+    </Box>
+  );
+}
